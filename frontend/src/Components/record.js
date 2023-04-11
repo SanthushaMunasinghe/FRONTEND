6ce3ec8fd9f09e4307cb192b90dd86d9958ab6a3
Avin Divakara <divakaraavin@gmail.com>
2023-04-11 10:20:07 +0530

    Record component and recording component

--- frontend/src/Components/record.js
+++ frontend/src/Components/record.js
@@ -1,18 +1,18 @@
 import React from "react";
-import "./recording.css";
+import "./record.css";
 import { FaPlayCircle } from "react-icons/fa";
 
-function RecordingComponent() {
+function RecordComponent() {
   return (
     <div className="container">
       <div className="container-record">
         <div className="record-content d-flex flex-column align-items-center justify-content-center">
           <FaPlayCircle className="record-icon" />
-          <h5 className="text-center">Listening for music...</h5>
+          <h5 className="text-center">Tap To Start</h5>
         </div>
       </div>
     </div>
   );
 }
 
-export default RecordingComponent;
+export default RecordComponent;
