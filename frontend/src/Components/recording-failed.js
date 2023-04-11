ad5156157a467e3692445cbccd49cae28874cd53
Avin Divakara <divakaraavin@gmail.com>
2023-04-11 10:20:49 +0530

    route back

--- frontend/src/Components/recording-failed.js
+++ frontend/src/Components/recording-failed.js
@@ -7,7 +7,7 @@ function RecordFailedComponent() {
       <div className="container-record">
         <div className="record-content d-flex flex-column align-items-center justify-content-center">
           <h5 className="text-center">Tap To Start</h5>
-          <button className="btn btn-dark mt-3">Try Again</button>
+          <a href="/"><button className="btn btn-dark mt-3">Try Again</button></a>
         </div>
       </div>
     </div>
