4a3950043b4feb295fdf1e662761eeac59726dd8
Shavinda Wanniarachchi <87648105+WKIShavinda@users.noreply.github.com>
2023-04-16 12:44:59 +0530

    update the h5 text

--- frontend/src/Components/recording-failed.js
+++ frontend/src/Components/recording-failed.js
@@ -6,7 +6,7 @@ function RecordFailedComponent(props) {
     <div className="container">
       <div className="container-record">
         <div className="record-content d-flex flex-column align-items-center justify-content-center">
-          <h5 className="text-center">Tap To Start</h5>
+          <h5 className="text-center">No Result</h5>
           <button className="btn btn-dark mt-3" onClick={() => props.retryFunction()}>Try Again</button>
         </div>
       </div>
